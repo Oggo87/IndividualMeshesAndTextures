653132c150716f05009c9e5d40ed3e8540f27053
Diego Noriega <dmnoriega13@gmail.com>
2024-08-26 23:42:34 -0400

    Added fall-back for helmet textures Added comments for asm code for better clarity

--- dllmain.cpp
+++ dllmain.cpp
@@ -55,42 +55,62 @@ DWORD meshNotExists = false;
 char* extension = NULL;
 char textureFileName[128];
 
+void prepFileNameString(DWORD &fileNameStr)
+{
+    //comparison has to include subfolders within cars.wad (i.e. cars\ in this case)
+    strcpy_s(textureFileName, 128, (char*)carsFolderStr);
+    strcat_s(textureFileName, 128, (char*)fileNameStr);
+
+    //extension needs to be tex for the comparison to work
+    extension = strstr(textureFileName, ".tga");
+    if (extension != NULL)
+    {
+        extension[2] = 'e';
+        extension[3] = 'x';
+    }
+
+    fileNameStr = PtrToUlong(textureFileName);
+}
+
 __declspec(naked) void genericMeshPerTrack() {
     
     __asm { //load trackIndex and push into stack
         mov EAX, trackIndex
         mov EAX, dword ptr[EAX]
-        add EAX,0x1
+        add EAX,0x1 //make track index rage 1-17
         push EAX
     }
     __asm { //original instructions (adjusted for new var in Stack)
         lea EAX, [ESP + 0x28] //Original is + 0x24
-        push EDX
-        push EAX
-        push ECX
+        push EDX //LOD number
+        push EAX //filename template
+        push ECX //filename
         call ReplaceWildCards
-        //check if mesh exists
-        mov var1, EAX
+    }
+    __asm { //check if mesh exists
+        mov var1, EAX //save EAX register
         push 0
-        push dword ptr[ESP+0x4]
+        push dword ptr[ESP+0x4] //filename
         call MeshFileExists
-        mov meshNotExists, EAX
-        mov EAX, var1
+        mov meshNotExists, EAX //save comparison result
+        mov EAX, var1 //restore EAX register
     }
     //fall-back
     if (meshNotExists)
     {
-        _asm {
-            pop var1
-            pop var2
-            pop var3
-            pop var4
-            push 0x1
-            push var3
-            push var2
-            push var1
+        _asm { //save variables from stack
+            pop var1 //filename
+            pop var2 //filename template
+            pop var3 //LOD number
+            pop var4 //track index
+        }
+        _asm { //new stack for fall-back name
+            push 0x1 //default to track 1
+            push var3 //LOD number
+            push var2 //filename template
+            push var1 //filename
             call ReplaceWildCards
-            mov var1, ESP
+            mov var1, ESP //save filename
         }
 
         OutputDebugStringA("Reverting to default mesh [");
@@ -100,16 +120,16 @@ __declspec(naked) void genericMeshPerTrack() {
         OutputDebugStringA("]\n");
     }
     _asm { //pop modified stack
-        pop var1
-        pop var2
-        pop var3
-        pop var4
+        pop var1 //filename
+        pop var2 //filename template
+        pop var3 //LOD number
+        pop var4 //track index
     }
 
     _asm { //restore stack as it should be
-        push var3
-        push var2
-        push var1
+        push var3 //LOD number
+        push var2 //filename template
+        push var1 //filename
     }
     __asm jmp genericMeshJumpBackAddress //jump back into regular flow
 }
@@ -119,7 +139,7 @@ __declspec(naked) void individualMeshPerTrack() {
     __asm { //load trackIndex and push into stack
         mov EAX, trackIndex
         mov EAX, dword ptr[EAX]
-        add EAX, 0x1
+        add EAX, 0x1 //make track index rage 1-17
         push EAX
     }
     __asm { //original instructions (adjusted for new var in Stack and reordered to work with local variables)
@@ -131,46 +151,30 @@ __declspec(naked) void individualMeshPerTrack() {
         inc EDX
         add ECX, arrTeamNames
         mov teamName, ECX //save current team name string address
-        push EAX
-        push EDX
-        push ECX
+        push EAX //LOD number
+        push EDX //driver 1 / driver 2
+        push ECX //team name
         lea EAX, [ESP + 0x34] //Original is + 0x30
         lea ECX, [ESP + 0x74] //Original is + 0x70
-        push EAX
-        push ECX
+        push EAX //filename template
+        push ECX //filename
         call ReplaceWildCards
     }
-    //__asm { //original instructions (adjusted for new var in Stack)
-    //    lea EAX, [EDX + EBP * 0x1]
-    //    mov EDX, dword ptr[ESP + 0x478] //Original is + 0x474
-    //    inc EDX
-    //    add ECX, 0x643ba8
-    //    lea ESI, [EAX * 0x4 + 0x6442a8]
-    //    mov EAX, dword ptr[EAX * 0x4 + 0x6442a8]
-    //    push EAX
-    //    push EDX
-    //    push ECX
-    //    lea EAX, [ESP + 0x34] //Original is + 0x30
-    //    lea ECX, [ESP + 0x74] //Original is + 0x70
-    //    push EAX
-    //    push ECX
-    //    call ReplaceWildCards
-    //}
     _asm { //pop modified stack
-        pop var1
-        pop var2
-        pop var3
-        pop var4
-        pop var5
-        pop var6
+        pop var1 //filename
+        pop var2 //filename template
+        pop var3 //team name
+        pop var4 //driver 1 / driver 2
+        pop var5 //LOD number
+        pop var6 //track index
     }
 
     _asm { //restore stack as it should be
-        push var5
-        push var4
-        push var3
-        push var2
-        push var1
+        push var5 //LOD number
+        push var4 //driver 1 / driver 2
+        push var3 //team name
+        push var2 //filename template
+        push var1 //filename
     }
     __asm jmp individualMeshJumpBackAddress //jump back into regular flow
 }
@@ -180,49 +184,32 @@ __declspec(naked) void defaultMeshPerTrack() {
     __asm { //default trackIndex to 1 and push into stack
         push 1
     }
-    //__asm { //load trackIndex and push into stack
-    //    mov EAX, trackIndex
-    //    mov EAX, dword ptr[EAX]
-    //    add EAX, 0x1
-    //    push EAX
-    //}
     __asm { //original instructions (adjusted for new var in Stack)
         mov EAX, dword ptr[ESI]
         lea ECX, [ESP + 0x28] //Original is + 0x24
-        push EAX
-        push 0x1
+        push EAX //LOD number
+        push 0x1 //use driver 1 for default
         push teamName //use current team name 
         lea EDX, [ESP + 0x74] //Original is + 0x70
-        push ECX
-        push EDX
+        push ECX //filename template
+        push EDX //filename
         call ReplaceWildCards
     }
-    //__asm { //original instructions (adjusted for new var in Stack)
-    //    mov EAX, dword ptr[ESI]
-    //    lea ECX, [ESP + 0x28] //Original is + 0x24
-    //    push EAX
-    //    push 0x1
-    //    push 0x644edc
-    //    lea EDX, [ESP + 0x74] //Original is + 0x70
-    //    push ECX
-    //    push EDX
-    //    call ReplaceWildCards
-    //}
     _asm { //pop modified stack
-        pop var1
-        pop var2
-        pop var3
-        pop var4
-        pop var5
-        pop var6
+        pop var1 //filename
+        pop var2 //filename template
+        pop var3 //team name
+        pop var4 //driver 1 / driver 2
+        pop var5 //LOD number
+        pop var6 //track index
     }
 
     _asm { //restore stack as it should be
-        push var5
-        push var4
-        push var3
-        push var2
-        push var1
+        push var5 //LOD number
+        push var4 //driver 1 / driver 2
+        push var3 //team name
+        push var2 //filename template
+        push var1 //filename
     }
     __asm jmp defaultMeshJumpBackAddress //jump back into regular flow
 }
@@ -232,11 +219,11 @@ __declspec(naked) void cockpitTexturePerTrack() {
     __asm { //load trackIndex and push into stack
         mov EAX, trackIndex
         mov EAX, dword ptr[EAX]
-        add EAX, 0x1
+        add EAX, 0x1 //make track index rage 1-17
         push EAX
     }
 
-    __asm { //load driver 1/2 and push into stack
+    __asm { //load driver 1 / driver 2 and push into stack
         mov EAX, dword ptr[ESP+0xc]
         add EAX, 0x1
         push EAX
@@ -245,40 +232,30 @@ __declspec(naked) void cockpitTexturePerTrack() {
     __asm { //original instructions (adjusted for new var in Stack and reordered to work with local variables)
         mov EAX, dword ptr[ESP + 0xd4] //Original is + 0xcc
         lea ECX, [ESP + 0xdc] //Original + 0xd4
-        push EAX
+        push EAX //team name
         mov EAX, cockpitTexture
-        push EAX
-        push ECX
+        push EAX //filename template
+        push ECX //filename
         call ReplaceWildCards
-        //check if file exists
+    }
+    __asm { //save volatile registers and filename from stack
         mov var1, EAX
         mov var2, ECX
         mov var3, EDX
         mov EAX, dword ptr[ESP]
         mov var4, EAX
-        //mov EAX, 0x00a05c3c
-        //mov dword ptr[EAX], 0
     }
 
-    //comparison has to include subfolders within cars.wad (i.e. cars\ in this case)
-    strcpy_s(textureFileName, 128, (char*)carsFolderStr);
-    strcat_s(textureFileName, 128, (char*)var4);
-    var4 = PtrToUlong(textureFileName);
+    //prep file name string for comparison
+    prepFileNameString(var4);
 
-    //extension needs to be tex for the comparison to work
-    extension = strstr(textureFileName, ".tga");
-    if(extension != NULL)
-    {
-        extension[2] = 'e';
-        extension[3] = 'x';
-    }
-
-    __asm{
-        //push dword ptr[ESP + 0x4]
+    __asm { //check if file exists
         push 0
-        push var4
+        push var4 //filename
         call MeshFileExists
-        mov meshNotExists, EAX
+        mov meshNotExists, EAX //save comparison result
+    }
+    __asm { //restore volatile registers
         mov EAX, var1
         mov ECX, var2
         mov EDX, var3
@@ -287,19 +264,21 @@ __declspec(naked) void cockpitTexturePerTrack() {
     //fall-back
     if (meshNotExists)
     {
-        _asm {
-            pop var1
-            pop var2
-            pop var3
-            pop var4
-            pop var5
-            push 0x1
-            push var4
-            push var3
-            push var2
-            push var1
+        _asm { //save variables from stack
+            pop var1 //filename
+            pop var2 //filename template
+            pop var3 //team name
+            pop var4 //driver 1 / driver 2
+            pop var5 //track index
+        }
+        _asm { //new stack for fall-back name
+            push 0x1 //default to track 1
+            push var4 //driver 1 / driver 2
+            push var3 //team name
+            push var2 //filename template
+            push var1 //filename 
             call ReplaceWildCards
-            mov var1, ESP
+            mov var1, ESP //save filename
         }
 
         OutputDebugStringA("Reverting to default texture [");
@@ -309,17 +288,17 @@ __declspec(naked) void cockpitTexturePerTrack() {
         OutputDebugStringA("]\n");
     }
     _asm { //pop modified stack
-        pop var1
-        pop var2
-        pop var3
-        pop var4
-        pop var5
+        pop var1 //filename
+        pop var2 //filename template
+        pop var3 //team name
+        pop var4 //driver 1 / driver 2
+        pop var5 //track index
     }
 
     _asm { //restore stack as it should be
-        push var3
-        push var2
-        push var1
+        push var3 //team name
+        push var2 //filename template
+        push var1 //filename 
     }
     __asm jmp cockpitTextureJumpBackAddress //jump back into regular flow
 
@@ -330,37 +309,77 @@ __declspec(naked) void helmetTexture1PerTrack() {
     __asm { //load trackIndex and push into stack
         mov EAX, trackIndex
         mov EAX, dword ptr[EAX]
-        add EAX, 0x1
+        add EAX, 0x1 //make track index rage 1-17
         push EAX
     }
 
     __asm { //original instructions (adjusted for new var in Stack and reordered to work with local variables)
         mov ESI, dword ptr[ESP + 0x2ec] //Original is + 0x2e8
         and ESI, 0xff
-        push ESI
+        push ESI //driver number
         mov EAX, helmetTexture1
-        push EAX
-        push ECX
+        push EAX //filename template
+        push ECX //filename
         call ReplaceWildCards
-        //check if file exists
-        //mov var1, EAX
-        //mov var2, ECX
-        //mov var3, EDX
-        //mov EAX, dword ptr[ESP]
-        //mov var4, EAX
+    }
+    __asm { //save volatile registers and filename from stack
+        mov var1, EAX
+        mov var2, ECX
+        mov var3, EDX
+        mov EAX, dword ptr[ESP]
+        mov var4, EAX
     }
 
+    //prep file name string for comparison
+    prepFileNameString(var4);
+
+    __asm { //check if file exists
+        push 0
+        push var4 //filename
+        call MeshFileExists
+        mov meshNotExists, EAX //save comparison result
+    }
+    __asm { //restore volatile registers
+        mov EAX, var1
+        mov ECX, var2
+        mov EDX, var3
+    }
+
+    //fall-back
+    if (meshNotExists)
+    {
+        _asm { //save variables from stack
+            pop var1 //filename
+            pop var2 //filename template
+            pop var3 //driver number
+            pop var4 //track index
+        }
+        _asm { //new stack for fall-back name
+            push 0x1 //default to track 1
+            push var3 //driver number
+            push var2 //filename template
+            push var1 //filename
+            call ReplaceWildCards
+            mov var1, ESP //save filename
+        }
+
+        OutputDebugStringA("Reverting to default texture [");
+
+        OutputDebugStringA(*(char**)ULongToPtr(var1));
+
+        OutputDebugStringA("]\n");
+    }
     _asm { //pop modified stack
-        pop var1
-        pop var2
-        pop var3
-        pop var4
+        pop var1 //filename
+        pop var2 //filename template
+        pop var3 //driver number
+        pop var4 //track index
     }
 
     _asm { //restore stack as it should be
-        push var3
-        push var2
-        push var1
+        push var3 //driver number
+        push var2 //filename template
+        push var1 //filename
     }
     __asm jmp helmetTexture1JumpBackAddress //jump back into regular flow
 
@@ -371,37 +390,77 @@ __declspec(naked) void helmetTexture2PerTrack() {
     __asm { //load trackIndex and push into stack
         mov ECX, trackIndex
         mov ECX, dword ptr[ECX]
-        add ECX, 0x1
+        add ECX, 0x1 //make track index rage 1-17
         push ECX
     }
 
     __asm { //original instructions (adjusted for new var in Stack and reordered to work with local variables)
-        push ESI
+        push ESI //driver number
         mov ECX, helmetTexture2
-        push ECX
+        push ECX //filename template
         lea ECX, [ESP + 0xe0] //Original is + 0xd8
-        push ECX
+        push ECX //filename
         mov dword ptr[EBP + 0xa8], EAX
         call ReplaceWildCards
-        //check if file exists
-        //mov var1, EAX
-        //mov var2, ECX
-        //mov var3, EDX
-        //mov EAX, dword ptr[ESP]
-        //mov var4, EAX
+    }
+    __asm { //save volatile registers and filename from stack
+        mov var1, EAX
+        mov var2, ECX
+        mov var3, EDX
+        mov EAX, dword ptr[ESP]
+        mov var4, EAX
     }
 
+    //prep file name string for comparison
+    prepFileNameString(var4);
+
+    __asm { //check if file exists
+        push 0
+        push var4 //filename
+        call MeshFileExists
+        mov meshNotExists, EAX //save comparison result
+    }
+    __asm { //restore volatile registers
+        mov EAX, var1
+        mov ECX, var2
+        mov EDX, var3
+    }
+
+    //fall-back
+    if (meshNotExists)
+    {
+        _asm { //save variables from stack
+            pop var1 //filename
+            pop var2 //filename template
+            pop var3 //driver number
+            pop var4 //track index
+        }
+        _asm { //new stack for fall-back name
+            push 0x1 //default to track 1
+            push var3 //driver number
+            push var2 //filename template
+            push var1 //filename
+            call ReplaceWildCards
+            mov var1, ESP //save filename
+        }
+
+        OutputDebugStringA("Reverting to default texture [");
+
+        OutputDebugStringA(*(char**)ULongToPtr(var1));
+
+        OutputDebugStringA("]\n");
+    }
     _asm { //pop modified stack
-        pop var1
-        pop var2
-        pop var3
-        pop var4
+        pop var1 //filename
+        pop var2 //filename template
+        pop var3 //driver number
+        pop var4 //track index
     }
 
     _asm { //restore stack as it should be
-        push var3
-        push var2
-        push var1
+        push var3 //driver number
+        push var2 //filename template
+        push var1 //filename
     }
     __asm jmp helmetTexture2JumpBackAddress //jump back into regular flow
 
