d7838ba5f01c2311c48f1c094291559f9b59e0ff
node91 <simon@node91.com>
2026-02-04 00:05:14 +0100

    Renamed and decoupled

--- Patches/General.cpp
+++ Patches/General.cpp
@@ -10,7 +10,7 @@ namespace General
 	//General settings variables
 	bool disableCDCheck = false;
 	bool fix3DWheelsTreadMapping = false;
-	bool rotateInCockpitView = false;
+	bool fix3DWheelsCockpitView = false;
 	bool stWheelAdvancedCarShader = false;
 
 	//Target Addresses
@@ -70,12 +70,12 @@ namespace General
 		initD3DMatrixVariables();
 
 		//invert the Y axis for the tyre tread texture on the left wheels
-		if (collisionMeshIndex == 0x12 || collisionMeshIndex == 0x16)
+		if (fix3DWheelsTreadMapping && (collisionMeshIndex == 0x12 || collisionMeshIndex == 0x16))
 		{
 			(*d3dMatrix_0xc0)[1][1] = -1;
 		}
 		//rotate the tyre tread texture when in cockpit view
-		if (rotateInCockpitView && ptrMeshContainer == GP4MemLib::MemUtils::addressToValue<DWORD>(ptrCockpitWheels))
+		if (fix3DWheelsCockpitView && ptrMeshContainer == GP4MemLib::MemUtils::addressToValue<DWORD>(ptrCockpitWheels))
 		{
 			(*d3dMatrix_0xc0)[0][0] = -1;
 			(*d3dMatrix_0xc0)[1][1] = -(*d3dMatrix_0xc0)[1][1];
@@ -119,11 +119,11 @@ namespace General
 		// Rotate cockpit view for front wheels (1 = enabled, 0 = disabled)
 		try
 		{
-			rotateInCockpitView = iniSettings["Settings"]["RotateTreadsInCockpitView"].getAs<bool>();
+			fix3DWheelsCockpitView = iniSettings["Settings"]["Fix3DWheelsCockpitView"].getAs<bool>();
 		}
 		catch (exception ex) {}
 
-		OutputGP4PPDebugString("Rotate Treads in Cockpit View : " + string(rotateInCockpitView ? "Enabled" : "Disabled"));
+		OutputGP4PPDebugString("Rotate Treads in Cockpit View : " + string(fix3DWheelsCockpitView ? "Enabled" : "Disabled"));
 
 		// Enable Advanced Car Shader for in-cockpit steering wheel
 		try
@@ -146,7 +146,7 @@ namespace General
 		}
 
 		// Apply Fix 3D Wheels Tread Mapping Patch
-		if (fix3DWheelsTreadMapping)
+		if (fix3DWheelsTreadMapping || fix3DWheelsCockpitView)
 		{
 			//Re-route for wheel tread texture set matrix
 			MemUtils::rerouteFunction(wheelShaderSetMatrixStartAddress, PtrToUlong(wheelShaderSetMatrixFunc), VAR_NAME(wheelShaderSetMatrixFunc));
